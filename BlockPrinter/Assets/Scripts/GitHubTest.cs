c4bf5e547913ba8eae0c54c6342d8e4b2b1e45e0
sqrqmi <xinghong885@gmail.com>
2025-12-24 18:58:35 +0900

    GitHubTest3
    テスト用

---
+++ BlockPrinter/Assets/Scripts/GitHubTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BlockPrinter
+{
+    public class GitHubTest : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            Debug.Log("Testing");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

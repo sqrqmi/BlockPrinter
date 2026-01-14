e582df8d9a7a407436909d15efc8fce45b52045d
Dixchrulth <yanmasma1111@gmail.com>
2026-01-14 10:44:22 +0900

    DiscardInstances有効化
    ・同シーン内で繰り返し遊べるように

--- BlockPrinter/Assets/Scripts/RecordDisplay.cs
+++ BlockPrinter/Assets/Scripts/RecordDisplay.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using Util;
 
 namespace BlockPrinter.UserInterface
 {
@@ -9,10 +10,28 @@ namespace BlockPrinter.UserInterface
 
     public class RecordDisplay : MonoBehaviour
     {
+        [SerializeField] private BitmapText ScoreText;
+        [SerializeField] private BitmapText PlayTimeText;
+        [SerializeField] private BitmapText PlacedBlockCountText;
+
+        private RecordInfo CurrentRecord;
 
         public void UpdateRecord(RecordInfo Record)
         {
-            
+            CurrentRecord = Record;
+
+            if(ScoreText != null)
+            {
+                ScoreText.UpdateText(CurrentRecord.Score.ToString());
+            }
+            if(PlayTimeText != null)
+            {
+                PlayTimeText.UpdateText(CurrentRecord.PlayTime.ToString());
+            }
+            if(PlacedBlockCountText != null)
+            {
+                PlacedBlockCountText.UpdateText(CurrentRecord.PlacedBlockCount.ToString());
+            }
         }
     }
 }

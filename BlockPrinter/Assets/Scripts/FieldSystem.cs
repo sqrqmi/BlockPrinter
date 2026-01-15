b41a6e6d2bc212de191399845df68c7402ad0319
Dixchrulth <yanmasma1111@gmail.com>
2026-01-15 14:51:39 +0900

    めも

--- BlockPrinter/Assets/Scripts/FieldSystem.cs
+++ BlockPrinter/Assets/Scripts/FieldSystem.cs
@@ -818,7 +818,7 @@ namespace BlockPrinter
                     }
                 }
                 CurrentDamagedBlockCount += BlockSum;
-                DamageDisplay.UpComingDamageBlocks(DamagedBlocks, new int[] { RedCount, BlueCount, GreenCount, YellowCount });
+                DamageDisplay.UpComingDamagedBlocks(DamagedBlocks, new int[] { RedCount, BlueCount, GreenCount, YellowCount }); // Kore Hairetuno suuchi tte zennbu zeroni natta atono degarasi jann!!
             }
             DamageDisplay.UpdateRemainingTime(CurrentDamageRemainingTime);
             DamageDisplay.SetRemainingTimeVisible(true);
